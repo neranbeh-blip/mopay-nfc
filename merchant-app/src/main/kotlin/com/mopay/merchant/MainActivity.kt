32272fb6feb0a59b1e52f8d7b3d50775f037b0dc
neranbeh-blip <neranbeh@gmail.com>
2026-09-20 20:40:38 +0100

    Add files via upload

--- merchant-app/src/main/kotlin/com/mopay/merchant/MainActivity.kt
+++ merchant-app/src/main/kotlin/com/mopay/merchant/MainActivity.kt
@@ -11,6 +11,7 @@ import android.nfc.tech.Ndef
 import android.os.Bundle
 import android.text.InputType
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
